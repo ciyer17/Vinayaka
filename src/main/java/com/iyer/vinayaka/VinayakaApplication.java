ed320c6d98b51460b9af89f07748f12369fbc924
gr33npo1s0n <iyerchandramouli03@gmail.com>
2024-12-29 01:14:42 -0800

    Initial Setup

---
+++ src/main/java/com/iyer/vinayaka/VinayakaApplication.java
@@ -0,0 +1,13 @@
+package com.iyer.vinayaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinayakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinayakaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iyer/vinayaka/VinayakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iyer.vinayaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinayakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
